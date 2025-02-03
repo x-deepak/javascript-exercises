4bf37cf027e78d9e1a479c23ee98505047b0d5f0
Deepak Kumar <142822780+x-deepak@users.noreply.github.com>
2025-02-03 17:43:38 +0530

    excersices 6 & 7 DONE

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const ans = (fahrenheit-32) * 5/9;
+  if (Number.isInteger(ans)) return ans;
+  return +ans.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const ans = celsius*9/5 +32;
+  if (Number.isInteger(ans)) return ans;
+  return +ans.toFixed(1);
 };
 
 // Do not edit below this line
